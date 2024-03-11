7be88ccd0a70a2f0d3c4a724f199fa05b4522091
Andrei Belov <abelov@student.42london.com>
2024-03-11 00:00:28 +0000

    c08

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelov <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 22:52:37 by abelov            #+#    #+#             */
+/*   Updated: 2023/10/26 22:52:39 by abelov           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# include <unistd.h>
+# define FT_BOOLEAN_H
+# undef TRUE
+# define TRUE    1
+# undef FALSE
+# define FALSE   0
+# define EVEN(a) ((a % 2) ? FALSE : TRUE)
+# define ODD_MSG ("I have an odd number of arguments.\n")
+# define EVEN_MSG ("I have an even number of arguments.\n")
+# define SUCCESS (0)
+
+typedef int	t_bool;
+#endif //FT_BOOLEAN_H
---
+++ c08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelov <abelov@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 23:41:13 by abelov            #+#    #+#             */
+/*   Updated: 2024/03/10 22:44:52 by abelov           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}				t_stock_str;
+
+static inline int	ft_strlen(char *str)
+{
+	const char	*ostr = str;
+
+	while (*str)
+		str++;
+	return (str - ostr);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	char *const	save = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (save);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*new;
+
+	new = malloc(sizeof(char) * ft_strlen(src) + 1);
+	if (!new)
+		return (NULL);
+	return (ft_strcpy(new, src));
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int			i;
+	t_stock_str	*stock;
+
+	i = 0;
+	stock = (t_stock_str *) malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!stock)
+		return (NULL);
+	while (i < ac)
+	{
+		stock[i] = (t_stock_str){
+			.size = ft_strlen(av[i]),
+			.str = av[i],
+			.copy = ft_strdup(av[i])
+		};
+		i++;
+	}
+	stock[i] = (t_stock_str){
+		.size = 0,
+		.str = 0,
+		.copy = 0
+	};
+	return (stock);
+}
