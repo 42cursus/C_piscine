fb36c7456aace594039f5b5a58c282cc052414ee
Andrei Belov <abelov@student.42london.com>
2024-03-17 02:11:20 +0000

    C09: fix norminette

--- c09/ex01/includes/ft.h
+++ c09/ex01/includes/ft.h
@@ -5,18 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abelov <abelov@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/10 22:48:47 by abelov            #+#    #+#             */
-/*   Updated: 2024/03/10 22:48:48 by abelov           ###   ########.fr       */
+/*   Created: 2023/10/26 22:49:42 by abelov            #+#    #+#             */
+/*   Updated: 2023/10/26 22:49:50 by abelov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_H
 # define FT_H
 
-void ft_putchar(char c);
-void ft_swap(int *a, int *b);
-void ft_putstr(char *str);
-int ft_strlen(char *str);
-int ft_strcmp(char *s1, char *s2);
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
 
 #endif //FT_H
